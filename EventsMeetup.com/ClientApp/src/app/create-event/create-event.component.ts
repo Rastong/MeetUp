1df5db9ebd7622054df1f280f85409b0caf9b03e
Kas Barber <kaseanbarber02@gmail.com>
2021-11-24 13:41:25 -0500

    added form for adding an event

--- EventsMeetup.com/ClientApp/src/app/create-event/create-event.component.ts
+++ EventsMeetup.com/ClientApp/src/app/create-event/create-event.component.ts
@@ -1,4 +1,7 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { EventService } from '../event.service';
+import { EventList } from '../EventList';
 
 @Component({
     selector: 'app-create-event',
@@ -8,7 +11,24 @@
 /** CreateEvent component*/
 export class CreateEventComponent {
     /** CreateEvent ctor */
-    constructor() {
+    constructor(private service:EventService) {
 
-    }
+  }
+  addToEvents(form: NgForm): any {
+    let result: EventList = {
+      id:0,
+      name: form.form.value.Name,
+      host: form.form.value.Host,
+      categoryID: form.form.value.CategoryID,
+      summary: form.form.value.Summary,
+      photo: form.form.value.Photo,
+      dateAndTime: form.form.value.DateAndTime,
+      guestCount: form.form.value.GuestCount,
+      spotsFilled: form.form.value.SpotsFilled
+    };
+    this.service.postEvent(result).subscribe((response: any) => {
+      console.log(response);
+    });
+
+  }
 }
--- EventsMeetup.com/ClientApp/src/app/event.service.ts
+++ EventsMeetup.com/ClientApp/src/app/event.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@angular/core';
+import { Host, Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { EventList } from './EventList';
 import { NgForm } from '@angular/forms';
@@ -7,6 +7,7 @@ import { NgForm } from '@angular/forms';
   providedIn: 'root'
 })
 export class EventService {
+  formData: EventList;
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
 
   }
@@ -22,8 +23,8 @@ export class EventService {
   }
 
   //addEvent
-  postEvent(event: EventList) {
-    return this.http.post(this.baseUrl + 'api/Events/AddEvent', event);
+  postEvent(formData: EventList) {
+    return this.http.post(this.baseUrl + `api/Events/AddEvent?name=${formData.name}&host=${formData.host}&categoryID=${formData.categoryID}&summary=${formData.summary}&photo=${formData.photo}&guestCount=${formData.guestCount}&dateAndTime=${formData.dateAndTime}&spotsFilled=${formData.spotsFilled}`, {});
   }
  
   
